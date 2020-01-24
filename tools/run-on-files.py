7c1d2c3ed621f4e4eb08bfc391bbcd134d274f4d
vbardiovsky <vbardiovsky@google.com>
2020-01-24 16:30:13 +0100

    Add check for submit.

--- tools/run-on-files.py
+++ tools/run-on-files.py
@@ -4,5 +4,9 @@ def main(argv):
   print("Running...")
   print(argv)
 
+  for filename in argv[1:]:
+    if "submit" not in filename:
+      raise ValueError("Must contain 'submit' in name.")
+
 if __name__ == '__main__':
   main(sys.argv)
